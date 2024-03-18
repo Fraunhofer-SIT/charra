50e6e14c43cce2a2852a86766129a26466f8810e
Markus Horn <markus.horn@sit.fraunhofer.de>
2024-03-18 10:12:30 +0100

    attester: verifier: charra_key_mgr: Updated attestation by using charra_tap
    - Updated data transfer objects
    - The attestation key will now be loaded from an file
    - The public information of the attestation key will now be loaded from an file
    
    Signed-off-by: Markus Horn <markus.horn@sit.fraunhofer.de>

--- src/core/charra_key_mgr.h
+++ src/core/charra_key_mgr.h
@@ -28,7 +28,7 @@
 #include "../common/charra_error.h"
 
 CHARRA_RC charra_load_tpm2_key(ESYS_CONTEXT* ctx, const uint32_t key_len,
-        const uint8_t* key, ESYS_TR* key_handle, TPM2B_PUBLIC** out_public);
+        const uint8_t* key, ESYS_TR* key_handle);
 
 CHARRA_RC charra_load_external_public_key(ESYS_CONTEXT* ctx,
         TPM2B_PUBLIC* external_public_key, ESYS_TR* key_handle);
